92c8fd0dfed4be7009e23f685d9645b2d2a6d329
Ahmed Khusaam <hello@khusaam.com>
2023-07-07 03:48:51 +0500

    - added functions to class scope - added return types - minor code refactor (switch to match)

--- src/EloquentFilterResolver.php
+++ src/EloquentFilterResolver.php
@@ -6,176 +6,174 @@ use Illuminate\Database\Eloquent\Builder;
 
 class EloquentFilterResolver
 {
-    public static function resolve(Builder &$query, string $filterString)
+    public static function resolve(Builder &$query, string $filterString): Builder
     {
-        $tokens = getTokens($filterString);
+        $tokens = self::getTokens($filterString);
         $index = 0;
 
-        return resolveFilter($query, $tokens, $index);
+        return self::resolveFilter($query, $tokens, $index);
     }
-}
 
-function getTokens(string $filterString): array
-{
-    $tokens = [];
-    $token = '';
-    $inString = false;
+    private static function getTokens(string $filterString): array
+    {
+        $tokens = [];
+        $token = '';
+        $inString = false;
 
-    for ($i = 0; $i < strlen($filterString); $i++) {
-        $c = $filterString[$i];
+        for ($i = 0; $i < strlen($filterString); $i++) {
+            $c = $filterString[$i];
 
-        if ($c === '"') {
-            $inString = !$inString;
-            if (!$inString) {
-                $tokens[] = $token;
+            if ($c === '"') {
+                $inString = !$inString;
+                if (!$inString) {
+                    $tokens[] = $token;
+                    $token = '';
+                }
+            } else if ($inString) {
+                if ($c === '\\' && $i + 1 <= strlen($filterString) && $filterString[$i + 1] === '"') {
+                    $token .= '"';
+                    $i++;
+                } else {
+                    $token .= $c;
+                }
+            } else if ($c === '(' || $c === ')' || $c === ' ' || $c === ',') {
+                if ($token !== '') {
+                    $tokens[] = $token;
+                }
                 $token = '';
-            }
-        } else if ($inString) {
-            if ($c === '\\' && $i + 1 <= strlen($filterString) && $filterString[$i + 1] === '"') {
-                $token .= '"';
-                $i++;
+
+                if ($c === '(' || $c === ')') {
+                    $tokens[] = $c;
+                }
             } else {
                 $token .= $c;
             }
-        } else if ($c === '(' || $c === ')' || $c === ' ' || $c === ',') {
-            if ($token !== '') {
-                $tokens[] = $token;
-            }
-            $token = '';
+        }
 
-            if ($c === '(' || $c === ')') {
-                $tokens[] = $c;
-            }
+        if ($token !== '') {
+            $tokens[] = $token;
+        }
+
+        return $tokens;
+    }
+
+    private static function parseQuery(Builder &$query, array $tokens, int &$index, $operand = 'AND'): void
+    {
+        $condition = self::getCondition($tokens[$index]);
+        $index += 2; // Skip opening '('
+        $field = $tokens[$index];
+        $index++;
+        $value = $tokens[$index];
+        $index += 2; // Skip closing ')'
+
+        $value = match ($value) {
+            'true' => true,
+            'false' => false,
+            'null' => null,
+            default => trim($value, '"'),
+        };
+
+        if ($condition === 'LIKE') {
+            $value = "%{$value}%";
+        } else if ($condition === 'IN' || $condition === 'NOT IN') {
+            $value = explode('|', $value);
+        }
+
+        $lastDot = strrpos($field, '.');
+
+        if ($lastDot !== false) {
+            $relation = substr($field, 0, $lastDot);
+            $field = substr($field, $lastDot + 1);
+            self::buildRelationQuery($query, $operand, $condition, $relation, $field, $value);
         } else {
-            $token .= $c;
+            self::buildQuery($query, $operand, $condition, $field, $value);
         }
     }
 
-    if ($token !== '') {
-        $tokens[] = $token;
+    private static function buildRelationQuery(Builder &$query, $operand, $condition, $relation, $field, $value): void
+    {
+        $relationMethod = 'whereHas';
+        if ($operand === 'OR') $relationMethod = 'orWhereHas';
+
+        $query->$relationMethod($relation, fn($q) => buildQuery($q, 'AND', $condition, $field, $value));
     }
 
-    return $tokens;
-}
+    private static function buildQuery(Builder &$query, $operand, $condition, $field, $value): void
+    {
+        $queryMethod = 'where';
+        if ($operand === 'OR') $queryMethod = 'orWhere';
 
-function parseQuery(Builder &$query, array $tokens, int &$index, $operand = 'AND') {
-    $condition = getCondition($tokens[$index]);
-    $index += 2; // Skip opening '('
-    $field = $tokens[$index];
-    $index++;
-    $value = $tokens[$index];
-    $index += 2; // Skip closing ')'
+        if ($condition === 'IN') $queryMethod .= 'In';
+        else if ($condition === 'NOT IN') $queryMethod .= 'NotIn';
 
-    switch ($value) {
-        case 'true':
-            $value = true;
-            break;
-        case 'false':
-            $value = false;
-            break;
-        case 'null':
-            $value = null;
-            break;
-        default:
-            $value = trim($value, '"');
-    }
-
-    if ($condition === 'LIKE') {
-        $value = "%{$value}%";
-    } else if ($condition === 'IN' || $condition === 'NOT IN') {
-        $value = explode('|', $value);
-    }
-
-    $lastDot = strrpos($field, '.');
-
-    if ($lastDot !== false) {
-        $relation = substr($field, 0, $lastDot);
-        $field = substr($field, $lastDot + 1);
-        buildRelationQuery($query, $operand, $condition, $relation, $field, $value);
-    } else {
-        buildQuery($query, $operand, $condition, $field, $value);
-    }
-}
-
-function buildRelationQuery(Builder &$query, $operand, $condition, $relation, $field, $value) {
-    $relationMethod = 'whereHas';
-    if ($operand === 'OR') $relationMethod = 'orWhereHas';
-
-    $query->$relationMethod($relation, fn ($q) => buildQuery($q, 'AND', $condition, $field, $value));
-}
-
-function buildQuery(Builder &$query, $operand, $condition, $field, $value) {
-    $queryMethod = 'where';
-    if ($operand === 'OR') $queryMethod = 'orWhere';
-
-    if ($condition === 'IN') $queryMethod .= 'In';
-    else if ($condition === 'NOT IN') $queryMethod .= 'NotIn';
-
-    if ($condition === 'IN' || $condition === 'NOT IN') {
-        $query->$queryMethod($field, $value);
-    } else {
-        $query->$queryMethod($field, $condition, $value);
-    }
-}
-
-function resolveFilter(Builder &$query, array $tokens, int &$index)
-{
-
-    while ($index < count($tokens)) {
-        $token = $tokens[$index];
-
-        if ($token === ')') {
-            break;
+        if ($condition === 'IN' || $condition === 'NOT IN') {
+            $query->$queryMethod($field, $value);
+        } else {
+            $query->$queryMethod($field, $condition, $value);
         }
+    }
 
-        $operand = 'AND';
+    private static function resolveFilter(Builder &$query, array $tokens, int &$index): Builder
+    {
 
-        $upperToken = strtoupper($token);
-        if ($upperToken === 'AND' || $upperToken === 'OR') {
-            $operand = $upperToken;
-            $index++;
+        while ($index < count($tokens)) {
             $token = $tokens[$index];
-        }
 
-        if ($token === '(') {
-            $index++;
-            if ($operand === 'AND') {
-                $query->where(function ($q) use ($tokens, &$index) {
-                    resolveFilter($q, $tokens, $index);
-                });
-            } else {
-                $query->orWhere(function ($q) use ($tokens, &$index) {
-                    resolveFilter($q, $tokens, $index);
-                });
+            if ($token === ')') {
+                break;
             }
-            $index++; // Skip the closing ')'
-            continue;
+
+            $operand = 'AND';
+
+            $upperToken = strtoupper($token);
+            if ($upperToken === 'AND' || $upperToken === 'OR') {
+                $operand = $upperToken;
+                $index++;
+                $token = $tokens[$index];
+            }
+
+            if ($token === '(') {
+                $index++;
+                if ($operand === 'AND') {
+                    $query->where(function ($q) use ($tokens, &$index) {
+                        self::resolveFilter($q, $tokens, $index);
+                    });
+                } else {
+                    $query->orWhere(function ($q) use ($tokens, &$index) {
+                        self::resolveFilter($q, $tokens, $index);
+                    });
+                }
+                $index++; // Skip the closing ')'
+                continue;
+            }
+
+            self::parseQuery($query, $tokens, $index, $operand);
         }
 
-        parseQuery($query, $tokens, $index, $operand);
+        return $query;
     }
 
-    return $query;
-}
+    private static function getCondition(string $condition): string
+    {
 
-function getCondition(string $contition): string
-{
+        $conditions = [
+            'eq' => '=',
+            'ne' => '!=',
+            'gt' => '>',
+            'lt' => '<',
+            'gte' => '>=',
+            'lte' => '<=',
+            'contains' => 'LIKE',
+            'in' => 'IN',
+            'notIn' => 'NOT IN',
+        ];
 
-    $conditions = [
-        'eq' => '=',
-        'ne' => '!=',
-        'gt' => '>',
-        'lt' => '<',
-        'gte' => '>=',
-        'lte' => '<=',
-        'contains' => 'LIKE',
-        'in' => 'IN',
-        'notIn' => 'NOT IN',
-    ];
+        if (array_key_exists($condition, $conditions)) {
+            return $conditions[$condition];
+        }
 
-    if (array_key_exists($contition, $conditions)) {
-        return $conditions[$contition];
+        throw new \InvalidArgumentException("Invalid condition: {$condition}");
     }
-
-    throw new \InvalidArgumentException("Invalid condition: {$contition}");
 }
+
+
